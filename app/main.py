202d95d2a4691dd4ff34ea6be7f3859990344f35
HieuITMHG <danghuuhieu038@gmail.com>
2026-03-29 15:59:42 +0700

    fix project name

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
 
 app = FastAPI(
-    title=settings.project_name,
+    title=settings.compose_project_name,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
