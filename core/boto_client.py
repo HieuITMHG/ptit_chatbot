0bc2f5e36b0890c65e917d578f9ec51e2e385b82
HieuITMHG <danghuuhieu038@gmail.com>
2026-04-30 09:07:16 +0700

    fix bucket name

--- core/boto_client.py
+++ core/boto_client.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.config import Config
 from core.config import settings
+from botocore.exceptions import ClientError
 
 s3 = boto3.client(
     's3',
@@ -10,3 +11,11 @@ s3 = boto3.client(
     config=Config(s3={'addressing_style': 'path'}),
     region_name="us-east-1",
 )
+
+def ensure_bucket(s3, bucket_name):
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+    except ClientError:
+        s3.create_bucket(Bucket=bucket_name)
+
+ensure_bucket(s3, settings.bucket_name)
