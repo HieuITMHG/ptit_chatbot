08f8de0c866b9a73ffb94d547fcd6bca94e7ac77
HieuITMHG <danghuuhieu038@gmail.com>
2026-04-30 08:46:35 +0700

    fix s3 credentials

--- core/boto_client.py
+++ core/boto_client.py
@@ -7,5 +7,6 @@ s3 = boto3.client(
     endpoint_url=settings.s3_endpoint,
     aws_access_key_id=settings.aws_access_key_id,         
     aws_secret_access_key=settings.aws_secret_access_key,  
-    config=Config(s3={'addressing_style': 'path'})
+    config=Config(s3={'addressing_style': 'path'}),
+    region_name="us-east-1",
 )
